8ae1c444d11395d41958a72a9bfea9f50f601699
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2022-03-22 09:09:51 +0700

    add

---
+++ src/main/java/cg/ExamModule4Application.java
@@ -0,0 +1,13 @@
+package cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamModule4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamModule4Application.class, args);
+    }
+
+}
